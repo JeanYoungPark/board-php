906cd5c4ca5f1d6f76fe11e19a079ba7aa5598e0
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-09-22 22:21:32 +0900

    기본 table생성과 mysql 연결 테스트

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require_once('view/top.php') ?>
+<div class="wrapper">
+    <table>
+        <thead>
+            <th></th>
+            <th>제목</th>
+            <th>작성일</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="number">1</td>
+                <td class="title"><a href="">이것은 제목입니다.</a></td>
+                <td class="date">2020-09-22</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<?php require_once('view/bottom.php') ?>
+    
