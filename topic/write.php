c2c91f04d0d2f34a30a4ff70fcd936baed377f71
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-09-27 04:19:46 +0900

    css 수정

--- topic/write.php
+++ topic/write.php
@@ -2,13 +2,11 @@
 require_once($_SERVER["DOCUMENT_ROOT"].'/class/html.php');
 $body =<<<JYP
 <div class="container">
-    <div class="form-group">
-        <form action="/process/write_process.php" method="post">
-        <p><input class="form-control" name="title" type="text" placeholder="제목"></p>
-        <p><textarea class="form-control" name="content" placeholder="내용"></textarea></p>
-        <p><input type="submit"></p>
-        </form>
-    </div>
+    <form action="/process/write_process.php" method="post">
+    <div class="form-group"><input class="form-control" name="title" type="text" placeholder="제목"></div>
+    <div class="form-group"><textarea class="form-control" name="content" placeholder="내용"></textarea></div>
+    <p><input type="submit"></p>
+    </form>
 </div>
 JYP;
 
