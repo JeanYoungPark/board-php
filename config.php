95218191ab58cd8a8af21436d11322b705e20aa6
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-10-11 18:11:08 +0900

    fix phpsessid 쿠키가 없을때의 오류 확인 후 수정

--- config.php
+++ config.php
@@ -8,5 +8,6 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/class/mysql.php');
 $mysql = new mysql;
 
 /*-------user--------*/
+$_COOKIE["PHPSESSID"] = '';
 require_once($_SERVER["DOCUMENT_ROOT"].'/user.php');
 ?>
